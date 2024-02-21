b650238387d73cbc0a64bb1bf3aebbc448dbcf0b
Arun Chouhan <108116902+arunchouhan07@users.noreply.github.com>
2024-02-21 12:54:20 +0530

    Delete employee-management-system directory

--- employee-management-system/src/main/java/com/hotwax/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotwax.repository;
-
-import com.hotwax.Entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
